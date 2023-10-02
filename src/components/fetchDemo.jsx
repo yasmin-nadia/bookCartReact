1bdfb280dd8e41c6263615f754f7d741991fb98e
Yasmin Nadia <yasmin.nadia@bjitacademy.com>
2023-10-02 16:29:21 +0600

    changes some css

--- src/components/fetchDemo.jsx
+++ src/components/fetchDemo.jsx
@@ -1,29 +1,31 @@
-import react, { useEffect } from "react";
+import React, { useEffect } from "react";
 import useProductHook from "../hooks/userProductHook";
 import "../App.css";
 
 const FetchDemo = () => {
   const { productData, loading } = useProductHook();
+
   useEffect(() => {
-    console.log("From fetchDemmo ", productData);
+    console.log("From FetchDemo", productData);
   }, [productData]);
+
   return (
     <div className="fetch-demo-container">
-      <h1>Fetch demo</h1>
-      <h3>Product data lists</h3>
-      {loading === true && <h4>Loading...</h4>}
-      <ul>
+      <h1>E-commerce Products</h1>
+      <div className="product-grid">
         {productData.map((product) => (
-          <li key={product._id}>
-            <h5>Title: {product.title}</h5>
+          <div className="product-item" key={product._id}>
+            <h5>{product.title}</h5>
             <p>Author: {product.author}</p>
-            <p>Price: {product.price}</p>
-            <p>Stock: {product.stock}</p>
-            <p>Genre: {product.genre.join(", ")}</p>
-          </li>
+            <p>Price: ${product.price}</p>
+            <p>Stock: {product.stock} in stock</p>
+            <p>Genres: {product.genre.join(", ")}</p>
+            <button className="add-to-cart-button">Add to Cart</button>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
+
 export default FetchDemo;
